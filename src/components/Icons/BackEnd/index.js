9e96b2495a6d9416f05ce8fd7b62c5d2454ac197
Larissa <larissasilvavarjao@gmail.com>
2020-03-17 14:35:52 -0300

    feat(icons): create backend icon

---
+++ src/components/Icons/BackEnd/index.js
@@ -0,0 +1,21 @@
+import { graphql, useStaticQuery } from "gatsby"
+import React from "react"
+import { DevWrapper } from "../FrontEnd/style"
+
+const BackEndIcon = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "backend.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <DevWrapper fluid={data.placeholderImage.childImageSharp.fluid} />
+}
+
+export default BackEndIcon
